cf60ed87927ee48e8ea32cbae0e24f48c60ddb2c
Kim Turley <kim@bynd.com>
2012-10-05 17:37:41 +0100

    changes

---
+++ js/app.build.js
@@ -0,0 +1,8 @@
+({
+	baseUrl: "app",
+	name: "Main",
+	out: "Main.js",
+	paths: {
+		jquery: "empty:"
+	}
+})
--- js/compressor.bat
+++ js/compressor.bat
@@ -1,2 +1,3 @@
-java -classpath plugins/js.jar;plugins/compiler.jar org.mozilla.javascript.tools.shell.Main plugins/r.js -o baseUrl=app name=Main out=app.min.js
+java -classpath plugins/js.jar;plugins/compiler.jar org.mozilla.javascript.tools.shell.Main plugins/r.js -o app.build.js
+java -classpath plugins/js.jar;plugins/compiler.jar org.mozilla.javascript.tools.shell.Main plugins/r.js -o cssIn=../css/app/Main.css out=../css/Main.css optimizeCss=Default
 pause
